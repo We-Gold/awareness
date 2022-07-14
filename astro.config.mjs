1b2428caa390c424b1b6048b6eec5ddafc6a6613
Weaver Goldman <we.goldm@gmail.com>
2022-07-14 18:57:36 -0400

    Removed the font optimizer and fixed the favicon import.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
 import { defineConfig } from 'astro/config';
-import fonts from 'astro-fonts-next'
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://We-Gold.github.io",
     base: "/awareness",
-    integrations: [
-        fonts({ url: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap' }),
-    ],
-    experimental: {
-        integrations: true,
-    }
 });
