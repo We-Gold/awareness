de4b6bede06cfef0f39cec6a46a63d045379796d
Weaver Goldman <we.goldm@gmail.com>
2022-07-14 18:24:45 -0400

    Added metadata and optimized fonts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
+import fonts from 'astro-fonts-next'
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://We-Gold.github.io",
-    base: "/awareness"
+    base: "/awareness",
+    integrations: [
+        fonts({ url: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap' }),
+    ],
+    experimental: {
+        integrations: true,
+    }
 });
