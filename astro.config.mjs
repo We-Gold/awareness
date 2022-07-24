c31e98dd5abe3f0501aea39c7eaf9d2b2f134554
Weaver Goldman <we-goldm@gmail.com>
2022-07-24 10:57:24 +0200

    Added astro optimization libraries.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
+import prefetch from "@astrojs/prefetch";
+import fonts from 'astro-fonts-next'
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://We-Gold.github.io",
-    base: "/awareness",
+  site: "https://We-Gold.github.io",
+  base: "/awareness",
+  integrations: [
+    fonts({ url: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap' }),
+    prefetch(), 
+  ],
+  experimental: {
+    integrations: true,
+  },
 });
